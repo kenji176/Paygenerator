86a86c9edf36a679ce5e71ca8140b6a12186a573
uta ko <masamunesasaki843@gmail.com>
2022-06-19 02:11:49 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', function () {
+    function generator_pay() {
+        let pay_link = ""
+        const text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        for (i = 0; i < num.value; i++) {
+            if (Numbrs) {
+                var randtext = Array.from(Array(8)).map(() => text[Math.floor(Math.random() * text.length)]).join('')
+                pay_link += "[" + i + "]" + "  https://pay.paypay.ne.jp/" + randtext + "\n"
+            } else {
+                var randtext = Array.from(Array(8)).map(() => text[Math.floor(Math.random() * text.length)]).join('')
+                pay_link += "https://pay.paypay.ne.jp/" + randtext + "\n"
+            }
+        }
+        navigator.clipboard.writeText(pay_link);
+    }
+
+    function change_setting() {
+        if (swi.checked) {
+            Numbrs = true
+        } else {
+            Numbrs = false
+        }
+    }
+    const btn = document.getElementById('btn');
+    const swi = document.getElementById('Switch');
+    const num = document.getElementById('Number');
+
+    let Numbrs = false;
+
+    swi.addEventListener('click', change_setting);
+    btn.addEventListener('click', generator_pay);
+})
