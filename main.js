27e61c037f03a161fcc823cd9bc6f5cb11f300bc
sinc <73232388+kenji176@users.noreply.github.com>
2022-06-19 06:30:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 window.addEventListener('DOMContentLoaded', function () {
+    function change_count() {
+        const counter = document.getElementById('count')
+        fetch("https://script.googleusercontent.com/macros/echo?user_content_key=4xiHZycjAcTakmti5OEBAgmlDnVDaHjdZD7a_WxMHvKDiULBQgfmytz9Qb4uPNdDlIoH0RqGl98NTiaMI-LoAeCcW5RbjJx8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMGUmVsXRxFQlDf_VZxp4m24gEMHkG6-Rqw0S7T0VsKXoh4_BZH3TefWFBqXP4ia01DKKcZoQPle&lib=M5cGVpYzPffJzlBS-LfugPf6sUuSqfdfD")
+            .then((res) => {
+                return (res.json());
+            })
+            .then((json) => {
+                counter.textContent = json.count;
+            });
+    }
+
     function generator_pay() {
+        fetch('https://script.google.com/macros/s/AKfycbxoabebpZAIknu_r1OEkvSZJYZSphNuChkiyyH2UIboZReDXfhc/exec', {
+            method: 'POST',
+            body: new URLSearchParams({
+                'count': num.value
+            })
+        });
         let pay_link = ""
         const text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         for (i = 0; i < num.value; i++) {
@@ -11,16 +28,9 @@ window.addEventListener('DOMContentLoaded', function () {
                 pay_link += "https://pay.paypay.ne.jp/" + randtext + "\n"
             }
         }
-        console.log(pay_link)
+        change_count()
         navigator.clipboard.writeText(pay_link);
-        document.querySelectorAll('.sample .toast')
-            .forEach(toastNode => {
-                const toast = new bootstrap.Toast(toastNode, {
-                    autohide: false
-                })
 
-                toast.show()
-            })
     }
 
     function change_setting() {
@@ -36,7 +46,16 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let Numbrs = false;
 
+    var toastElList = [].slice.call(document.querySelectorAll(".toast"));
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl, {
+            // オプション
+            delay: 60000,
+        });
+    });
+
 
     swi.addEventListener('click', change_setting);
-    btn.addEventListener('click', generator_pay);
+    btn.addEventListener('click', generator_pay, change_count());
+    change_count()
 })
