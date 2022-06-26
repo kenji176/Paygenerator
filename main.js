222b383f67d94d8f83792ad39b9695a7ae2a3fe9
sinc <73232388+kenji176@users.noreply.github.com>
2022-06-26 17:40:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function () {
     });
     function change_count() {
         const counter = document.getElementById('count')
-        fetch("https://script.googleusercontent.com/macros/echo?user_content_key=4xiHZycjAcTakmti5OEBAgmlDnVDaHjdZD7a_WxMHvKDiULBQgfmytz9Qb4uPNdDlIoH0RqGl98NTiaMI-LoAeCcW5RbjJx8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMGUmVsXRxFQlDf_VZxp4m24gEMHkG6-Rqw0S7T0VsKXoh4_BZH3TefWFBqXP4ia01DKKcZoQPle&lib=M5cGVpYzPffJzlBS-LfugPf6sUuSqfdfD")
+        fetch("https://script.google.com/macros/s/AKfycby_lD0RRBy6MaYSAe3jx2xdy1j3e_kcA-DiOOLAn8kUaDeShuvFj1Y3O01xJT0ClfRTjw/exec")
             .then((res) => {
                 return (res.json());
             })
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     function generator_pay(callback) {
-        fetch('https://script.google.com/macros/s/AKfycbxoabebpZAIknu_r1OEkvSZJYZSphNuChkiyyH2UIboZReDXfhc/exec', {
+        fetch('https://script.google.com/macros/s/AKfycby_lD0RRBy6MaYSAe3jx2xdy1j3e_kcA-DiOOLAn8kUaDeShuvFj1Y3O01xJT0ClfRTjw/exec', {
             method: 'POST',
             body: new URLSearchParams({
                 'count': Paypay['num'].value
